67428a18777971b86439424ca8a99e8609c0e023
llathasa-veleth <llathasa@outlook.com>
2022-08-03 21:05:46 +0300

    Bare minimum

--- lib/magnolia.ex
+++ lib/magnolia.ex
@@ -1,5 +1,17 @@
 defmodule Magnolia do
-  def main(_) do
-    IO.puts("hewwo")
+  defp prompt() do
+    # TODO
+  end
+
+  defp run_file(_src) do
+    # TODO
+  end
+
+  def main(args) do
+    case args do
+      []    -> prompt()
+      [src] -> run_file(src)
+      _     -> IO.puts("\x1B[93musage:\x1B[0m magnolia [file]")
+    end
   end
 end
